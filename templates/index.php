448fea28b4b1e85eef6d0c975571da23c3659605
lanurag150 <60335727+lanurag150@users.noreply.github.com>
2020-05-30 14:56:38 +0530

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -73,12 +73,12 @@
       </div>
     </div>
 
-   <section class="ftco-section contact-section ftco-degree-bg" style="height: 2500px">
+   <section class="ftco-section contact-section ftco-degree-bg" style="height: 2500px;background-color:AntiqueWhite">
    
       <div class="container col-lg-6" >
           
           
-            <div class="w3-card-4" style="width: 550px;">
+            <div class="w3-card-4" style="width: 550px;background-color:White">
               <div class="w3-container w3-black">
                 <h2 style="color:white;">Chronic Kidney Analysis</h2>
               </div>
